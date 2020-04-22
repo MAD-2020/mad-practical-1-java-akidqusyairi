062090552945256048f78830508d2c4cf6dcc77e
akidqusyairi <64066753+akidqusyairi@users.noreply.github.com>
2020-04-22 13:18:20 +0800

    Question3.java

--- Question3.java
+++ Question3.java
@@ -5,7 +5,7 @@ public class Question3
   public static void main(String[] args)
   {
     
-    Scanner number = new Scanner(System.in);
+    Scanner in = new Scanner(System.in);
     System.out.println("Enter an integer: ");
     int number = in.nextInt();
     int total = number*number;
