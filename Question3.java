7c1f3167d781dbfe7183842b927c4e86a9abfce9
akidqusyairi <64066753+akidqusyairi@users.noreply.github.com>
2020-04-22 13:06:14 +0800

    Quesstion3.java

--- Question3.java
+++ Question3.java
@@ -4,13 +4,12 @@ public class Question3
 {
   public static void main(String[] args)
   {
-    /**
-     * Reads integer entered by the user, multiplies the integer by itself and print out the result.
-     * 
-     * Hint: in.nextInt() to read integer
-     */
-     
-    Scanner in = new Scanner(System.in);
+    
+    Scanner number = new Scanner(System.in);
+    System.out.println("Enter an integer: ");
+    int number = in.nextInt();
+    int total = number*number;
+    System.out.println("Result: " + total);
     
   }
 }
